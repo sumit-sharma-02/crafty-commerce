dec37a9490d2d1cd13cdabc9490661c50fac2c87
Sumit <sumitsharma9968@gmail.com>
2022-12-08 23:41:11 +0530

    Added header in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import { AnimatePresence } from "framer-motion";
 import "./App.css";
 import { Header } from "./components";
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <AnimatePresence mode="wait">
-      <div className="w-screen h-auto flex flex-col bg-primary">
+      <div className="min-h-screen flex flex-col">
         <Header />
+        {/* <Routes></Routes> */}
+        <Footer />
       </div>
     </AnimatePresence>
   );
---
+++ frontend_bootstrap/src/App.js
@@ -0,0 +1,15 @@
+import Home from "./components/Home";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
