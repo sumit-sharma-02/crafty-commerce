d9a0860e02a8469ca552394c7a04d1844e999626
Sumit <sumitsharma9968@gmail.com>
2022-12-30 07:03:14 +0530

    Dashboard UI changes

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { Sidebar } from "../../components";
+import { TfiReceipt } from "react-icons/tfi";
+import { BsGraphDown } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { FiChevronRight, FiUsers } from "react-icons/fi";
+import { GiReceiveMoney } from "react-icons/gi";
 
 const Dashboard = () => {
   return (
@@ -17,9 +22,9 @@ const Dashboard = () => {
                     </h2>
                   </div>
                   <div className="mt-5 grid grid-cols-12 gap-6">
-                    <a
-                      className="intro-y  col-span-12 transform rounded-lg bg-white shadow-xl transition duration-300 hover:scale-105 sm:col-span-6 xl:col-span-3"
-                      href="#"
+                    <Link
+                      className="intro-y col-span-12 transform rounded-lg bg-white shadow-xl transition duration-300 hover:scale-105 sm:col-span-6 xl:col-span-3"
+                      to={"/admin/products"}
                     >
                       <div className="p-5">
                         <div className="flex justify-between">
@@ -37,143 +42,149 @@ const Dashboard = () => {
                               d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
                             />
                           </svg>
-                          <div className="flex h-6 justify-items-center rounded-full bg-green-500 px-2 text-sm font-semibold text-black">
-                            <span className="flex items-center">30%</span>
+                          <div className="flex h-6 items-center rounded-full bg-green-400 px-2 text-xs font-medium text-green-800">
+                            <span className="flex items-center">
+                              View Details
+                            </span>
+                            <span className="pt-[3px]">
+                              <FiChevronRight />
+                            </span>
                           </div>
                         </div>
                         <div className="ml-2 w-full flex-1">
                           <div>
                             <div className="mt-3 text-3xl font-bold leading-8">
-                              4.510
+                              56
                             </div>
 
                             <div className="mt-1 text-base text-gray-600">
-                              Item Sales
+                              Products
                             </div>
                           </div>
                         </div>
                       </div>
-                    </a>
-                    <a
+                    </Link>
+                    <Link
+                      className="intro-y  col-span-12 transform rounded-lg bg-white shadow-xl transition duration-300 hover:scale-105 sm:col-span-6 xl:col-span-3"
+                      to={"/admin/orders"}
+                    >
+                      <div className="p-5">
+                        <div className="flex justify-between">
+                          <TfiReceipt
+                            className="h-6 w-6 text-yellow-400"
+                            strokeWidth="0.8"
+                          />
+                          <div className="flex h-6 items-center rounded-full bg-blue-400 px-2 text-xs font-medium text-blue-800">
+                            <span className="flex items-center">
+                              View Details
+                            </span>
+                            <span className="pt-[3px]">
+                              <FiChevronRight />
+                            </span>
+                          </div>
+                        </div>
+                        <div className="ml-2 w-full flex-1">
+                          <div>
+                            <div className="mt-3 text-3xl font-bold leading-8">
+                              125
+                            </div>
+
+                            <div className="mt-1 text-base text-gray-600">
+                              Orders
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                    <Link
+                      className="intro-y  col-span-12 transform rounded-lg bg-white shadow-xl transition duration-300 hover:scale-105 sm:col-span-6 xl:col-span-3"
+                      to={"/admin/users"}
+                    >
+                      <div className="p-5">
+                        <div className="flex justify-between">
+                          <FiUsers
+                            className="h-6 w-6 text-pink-600"
+                            strokeWidth="2.5"
+                          />
+                          <div className="flex h-6 items-center rounded-full bg-yellow-400 px-2 text-xs font-medium text-yellow-800">
+                            <span className="flex items-center">
+                              View Details
+                            </span>
+                            <span className="pt-[3px]">
+                              <FiChevronRight />
+                            </span>
+                          </div>
+                        </div>
+                        <div className="ml-2 w-full flex-1">
+                          <div>
+                            <div className="mt-3 text-3xl font-bold leading-8">
+                              45
+                            </div>
+
+                            <div className="mt-1 text-base text-gray-600">
+                              Users
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                    <div
                       className="intro-y  col-span-12 transform rounded-lg bg-white shadow-xl transition duration-300 hover:scale-105 sm:col-span-6 xl:col-span-3"
                       href="#"
                     >
                       <div className="p-5">
                         <div className="flex justify-between">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-7 w-7 text-yellow-400"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth="2"
-                              d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                            />
-                          </svg>
-                          <div className="flex h-6 justify-items-center rounded-full bg-red-500 px-2 text-sm font-semibold text-black">
-                            <span className="flex items-center">30%</span>
-                          </div>
+                          <BsGraphDown
+                            strokeWidth={0.8}
+                            className="h-6 w-6 text-red-400"
+                          />
                         </div>
                         <div className="ml-2 w-full flex-1">
                           <div>
                             <div className="mt-3 text-3xl font-bold leading-8">
-                              4.510
+                              4
                             </div>
 
                             <div className="mt-1 text-base text-gray-600">
-                              Item Sales
+                              Out of Stock
                             </div>
                           </div>
                         </div>
                       </div>
-                    </a>
-                    <a
-                      className="intro-y  col-span-12 transform rounded-lg bg-white shadow-xl transition duration-300 hover:scale-105 sm:col-span-6 xl:col-span-3"
-                      href="#"
-                    >
-                      <div className="p-5">
-                        <div className="flex justify-between">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-7 w-7 text-pink-600"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth="2"
-                              d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"
-                            />
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth="2"
-                              d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"
-                            />
-                          </svg>
-                          <div className="flex h-6 justify-items-center rounded-full bg-yellow-500 px-2 text-sm font-semibold text-black">
-                            <span className="flex items-center">30%</span>
-                          </div>
-                        </div>
-                        <div className="ml-2 w-full flex-1">
-                          <div>
-                            <div className="mt-3 text-3xl font-bold leading-8">
-                              4.510
-                            </div>
-
-                            <div className="mt-1 text-base text-gray-600">
-                              Item Sales
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </a>
-                    <a
-                      className="intro-y  col-span-12 transform rounded-lg bg-white shadow-xl transition duration-300 hover:scale-105 sm:col-span-6 xl:col-span-3"
-                      href="#"
-                    >
-                      <div className="p-5">
-                        <div className="flex justify-between">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-7 w-7 text-green-400"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth="2"
-                              d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"
-                            />
-                          </svg>
-                          <div className="flex h-6 justify-items-center rounded-full bg-blue-500 px-2 text-sm font-semibold text-black">
-                            <span className="flex items-center">30%</span>
-                          </div>
-                        </div>
-                        <div className="ml-2 w-full flex-1">
-                          <div>
-                            <div className="mt-3 text-3xl font-bold leading-8">
-                              4.510
-                            </div>
-
-                            <div className="mt-1 text-base text-gray-600">
-                              Item Sales
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </a>
+                    </div>
                   </div>
                 </div>
+
                 <div className="col-span-12 mt-5">
+                  <div className="grid grid-cols-1 gap-2 lg:grid-cols-1">
+                    <div className="flex h-full w-full items-center justify-center">
+                      <div
+                        className="intro-y col-span-12 w-full transform rounded-lg bg-white shadow-xl transition duration-300 hover:scale-105 sm:col-span-6 xl:col-span-3"
+                        href="#"
+                      >
+                        <div className="p-5">
+                          <div className="flex justify-center">
+                            <GiReceiveMoney className="h-7 w-7 text-green-400" />
+                          </div>
+                          <div className="ml-2 flex w-full justify-center">
+                            <div>
+                              <div className="mt-3 text-3xl font-bold leading-8">
+                                $4567
+                              </div>
+
+                              <div className="mt-1 text-base text-gray-600">
+                                Total Amount
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div></div>
+                    </div>
+                  </div>
+                </div>
+
+                {/* <div className="col-span-12 mt-5">
                   <div className="grid grid-cols-1 gap-2 lg:grid-cols-1">
                     <div className="rounded-lg bg-white p-4 shadow-lg">
                       <h1 className="text-base font-bold">Table</h1>
@@ -293,7 +304,7 @@ const Dashboard = () => {
                       </div>
                     </div>
                   </div>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
