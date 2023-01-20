fea0efaa74715762ace60539c03027019f157758
sumit-sharma-02 <sumitsharma9968@gmail.com>
2023-01-21 01:15:25 +0530

    Minor bug fix

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ process.on("uncaughtException", (err) => {
 });
 
 // Setting up config file
-dotenv.config({ path: "backend/config/config.env" });
+if (process.env.NODE_ENV !== "PRODUCTION")
+  require("dotenv").config({ path: "backend/config/config.env" });
+// dotenv.config({ path: "backend/config/config.env" });
 
 // Connecting to database
 connectDatabase();
