2b6253a06d86bb4c2a91bfa589dc6099e8b2e171
sumit-sharma-02 <sumitsharma9968@gmail.com>
2023-01-20 18:33:56 +0530

    Added production build

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
+const path = require("path");
 
 const errorMiddleware = require("./middlewares/errors");
 
@@ -33,6 +34,13 @@ app.use(`/api/v1`, products);
 app.use(`/api/v1`, auth);
 app.use(`/api/v1`, payment);
 app.use(`/api/v1`, order);
+
+if (process.env.NODE_ENV === "PRODUCTION") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  });
+}
 
 // Middleware for Error Handling
 app.use(errorMiddleware);
